7b5e451ff9ec58e2de159476116f50ead0a36fff
денис эм <denisem@deniss-MacBook-Pro.local>
2021-12-05 17:42:22 +0300

    added password endoder overrided persist update methods in UserDao

--- src/main/java/com/javamentor/qa/platform/dao/impl/model/UserDao.java
+++ src/main/java/com/javamentor/qa/platform/dao/impl/model/UserDao.java
@@ -1,8 +1,31 @@
 package com.javamentor.qa.platform.dao.impl.model;
 
 import com.javamentor.qa.platform.models.entity.user.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public class UserDao extends ReadWriteDaoImpl<User,Long>{
+
+    private final PasswordEncoder passwordEncoder;
+
+    @Autowired
+    public UserDao(PasswordEncoder passwordEncoder){
+        this.passwordEncoder = passwordEncoder;
+    }
+
+    @Override
+    public void persist(User e) {
+        String password = e.getPassword();
+        e.setPassword(passwordEncoder.encode(password));
+        super.persist(e);
+    }
+
+    @Override
+    public void update(User e) {
+        String password = e.getPassword();
+        e.setPassword(passwordEncoder.encode(password));
+        super.update(e);
+    }
 }
