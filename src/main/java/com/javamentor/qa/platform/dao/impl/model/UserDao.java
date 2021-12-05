769ae97a944ae128c5696a997d1f51cbe7077d92
денис эм <denisem@deniss-MacBook-Pro.local>
2021-12-05 17:53:17 +0300

    added getUser(email) method in UserDao;

--- src/main/java/com/javamentor/qa/platform/dao/impl/model/UserDao.java
+++ src/main/java/com/javamentor/qa/platform/dao/impl/model/UserDao.java
@@ -5,14 +5,27 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Repository;
 
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.Query;
+
 @Repository
 public class UserDao extends ReadWriteDaoImpl<User,Long>{
 
     private final PasswordEncoder passwordEncoder;
 
+    @PersistenceContext
+    private EntityManager entityManager;
+
     @Autowired
     public UserDao(PasswordEncoder passwordEncoder){
         this.passwordEncoder = passwordEncoder;
+    }
+
+    public User getUser(String email){
+        Query query =  entityManager.createQuery("select u from User u where u.email=:email");
+        query.setParameter("email", email);
+        return (User) query.getSingleResult();
     }
 
     @Override
