c4d680f14b8bdd3bf6b251af9b9744c008d91b3f
Igor <88908528+iMarzhuhin@users.noreply.github.com>
2021-12-08 18:28:49 +0300

    pageDtp

---
+++ src/main/java/com/javamentor/qa/platform/models/dto/PageDtp.java
@@ -0,0 +1,35 @@
+package com.javamentor.qa.platform.models.dto;
+
+
+import java.util.List;
+
+
+public abstract class PageDtp<T, P> {
+
+    private int currentPageNumber;
+    private int totalPageCount;
+    private int totalResultCount;
+    private List<T> items;
+    private int itemsOnPage;
+
+    public PageDtp(int itemsOnPage, int currentPageNumber, P param) {
+        if (itemsOnPage > 0) {
+            this.itemsOnPage = itemsOnPage;
+        } else {
+            throw new IllegalArgumentException("Неверно выбрано количество записей на страницу");
+        }
+        this.currentPageNumber = currentPageNumber;
+        totalResultCount = getTotalResultCount(param);
+        totalPageCount = totalResultCount <= itemsOnPage ? 1
+                : (totalResultCount % itemsOnPage) == 0 ? totalResultCount / itemsOnPage
+                : (totalResultCount / itemsOnPage) + 1;
+        if (currentPageNumber > totalPageCount) {
+            throw new IllegalArgumentException("Страницы под номером "
+                    + currentPageNumber + " пока не существует");
+        }
+    }
+
+    public abstract List<T> getItems(P param);
+
+    public abstract int getTotalResultCount(P param);
+}
