09f024fb14274eea94532952fa7edb55ed5719d8
Igor <88908528+iMarzhuhin@users.noreply.github.com>
2021-12-08 18:49:02 +0300

    add getters&setters

--- src/main/java/com/javamentor/qa/platform/models/dto/PageDtp.java
+++ src/main/java/com/javamentor/qa/platform/models/dto/PageDtp.java
@@ -1,9 +1,13 @@
 package com.javamentor.qa.platform.models.dto;
 
 
+import lombok.Getter;
+import lombok.Setter;
+
 import java.util.List;
 
-
+@Getter
+@Setter
 public abstract class PageDtp<T, P> {
 
     private int currentPageNumber;
