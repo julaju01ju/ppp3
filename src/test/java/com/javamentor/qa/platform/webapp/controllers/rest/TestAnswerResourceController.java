c19c490be7f3b4646c3001b12558309fa91fcca0
денис эм <denisem@deniss-MacBook-Pro.local>
2021-12-17 17:38:39 +0300

    added auth into AnswerResourceControllerTest

--- src/test/java/com/javamentor/qa/platform/webapp/controllers/rest/TestAnswerResourceController.java
+++ src/test/java/com/javamentor/qa/platform/webapp/controllers/rest/TestAnswerResourceController.java
@@ -1,9 +1,17 @@
 package com.javamentor.qa.platform.webapp.controllers.rest;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.github.database.rider.core.api.dataset.DataSet;
+import com.javamentor.qa.platform.models.dto.AuthenticationRequest;
 import org.junit.jupiter.api.Test;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
+import static org.springframework.http.HttpHeaders.AUTHORIZATION;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -12,15 +20,30 @@ public class TestAnswerResourceController
         extends AbstractConrollersTests{
 
     @Test
-    @DataSet(value = {"dataset/AnswerResourceController/users.yml",
+    @DataSet(value = {
+            "dataset/AnswerResourceController/users.yml",
             "dataset/AnswerResourceController/answers.yml",
             "dataset/AnswerResourceController/questions.yml",
             "dataset/AnswerResourceController/reputations.yml",
-            "dataset/AnswerResourceController/roles.yml",
-            "dataset/AnswerResourceController/answervote.yml"
-    })
+            "dataset/AnswerResourceController/answervote.yml",
+    },  disableConstraints = true)
     public void getAllAnswerDtosByQustionId() throws Exception {
-        mockMvc.perform(MockMvcRequestBuilders.get("/api/user/question/102/answer"))
+        AuthenticationRequest authenticationRequest = new AuthenticationRequest();
+        authenticationRequest.setPassword("USER");
+        authenticationRequest.setUsername("user@mail.ru");
+
+        String USER_TOKEN = mockMvc.perform(
+                        post("/api/auth/token/")
+                                .content(new ObjectMapper().writeValueAsString(authenticationRequest))
+                                .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andReturn().getResponse().getContentAsString();
+
+        USER_TOKEN = "Bearer " + USER_TOKEN.substring(USER_TOKEN.indexOf(":") + 2, USER_TOKEN.length() - 2);
+        System.out.println(USER_TOKEN);
+        mockMvc.perform(
+                get("/api/user/question/102/answer")
+                        .header(AUTHORIZATION, USER_TOKEN))
                 .andDo(print())
                 .andExpect(status().isOk())
                 .andExpect(jsonPath("$[0].id").value(102))
@@ -32,8 +55,8 @@ public class TestAnswerResourceController
                 .andExpect(jsonPath("$[0].isHelpful").value("true"))
                 .andExpect(jsonPath("$[0].dateAccept").value("2021-12-06T03:00:00"))
                 .andExpect(jsonPath("$[0].countValuable").value(1))
-                .andExpect(jsonPath("$[0].image").value("link"))
-                .andExpect(jsonPath("$[0].nickName").value("NickName"));
+                .andExpect(jsonPath("$[0].image").value("image"))
+                .andExpect(jsonPath("$[0].nickName").value("USR"));
     }
 
     @Test
@@ -41,13 +64,29 @@ public class TestAnswerResourceController
             "dataset/AnswerResourceController/answers.yml",
             "dataset/AnswerResourceController/questions.yml",
             "dataset/AnswerResourceController/reputations.yml",
-            "dataset/AnswerResourceController/roles.yml",
-            "dataset/AnswerResourceController/answervote.yml"
+            "dataset/AnswerResourceController/answervote.yml",
     })
     void shouldNotGetAnswerByQuestionId() throws Exception {
-        mockMvc.perform(MockMvcRequestBuilders.get("/api/user/question/105/answer"))
+        AuthenticationRequest authenticationRequest = new AuthenticationRequest();
+        authenticationRequest.setPassword("USER");
+        authenticationRequest.setUsername("user@mail.ru");
+
+        String USER_TOKEN = mockMvc.perform(
+                        post("/api/auth/token/")
+                                .content(new ObjectMapper().writeValueAsString(authenticationRequest))
+                                .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andReturn().getResponse().getContentAsString();
+
+        USER_TOKEN = "Bearer " + USER_TOKEN.substring(USER_TOKEN.indexOf(":") + 2, USER_TOKEN.length() - 2);
+        System.out.println(USER_TOKEN);
+
+        mockMvc.perform(
+                        get("/api/user/question/105/answer")
+                                .header(AUTHORIZATION, USER_TOKEN))
                 .andDo(print())
                 .andExpect(status().isNotFound())
                 .andExpect(jsonPath("$.id").doesNotExist());
     }
+
 }
