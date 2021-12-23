61a0dcc2f92b1a10ad61fbb336c0e63c95770462
Yuriy Galkin <ygalkin85@gmail.com>
2021-12-23 16:51:00 +0300

    Added PaginationUser Changed UserDtoService, UserDtoServiceImpl Added getAllUsersOrderByPersistDatePagination in UserResourseController. Changed name TestUserResourseController to TestUserResourceController. Added token authorization in old tests.

--- src/main/java/com/javamentor/qa/platform/webapp/controllers/rest/UserResourseController.java
+++ src/main/java/com/javamentor/qa/platform/webapp/controllers/rest/UserResourseController.java
@@ -1,5 +1,7 @@
 package com.javamentor.qa.platform.webapp.controllers.rest;
 
+import com.javamentor.qa.platform.models.dto.PageDto;
+import com.javamentor.qa.platform.models.dto.UserDto;
 import com.javamentor.qa.platform.service.abstracts.dto.UserDtoService;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
@@ -8,7 +10,11 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.Map;
 
 
 /**
@@ -29,8 +35,22 @@ public class UserResourseController {
         return userDtoService.getUserById(userId).isEmpty() ?
                 new ResponseEntity<>("User with id " + userId + " not found!", HttpStatus.NOT_FOUND) :
                 new ResponseEntity<>(userDtoService.getUserById(userId), HttpStatus.OK);
-
     }
 
+    @GetMapping("/api/user/new")
+    @ApiOperation("API получение всех пользователей, отсортированных по дате регистрации, с пагинацией. " +
+            "Принимает параметры: page(обязательный) - текущая страница и " +
+            "items(необязательный) - количество элементов на страницу. По умолчанию равен 10.")
+    public ResponseEntity<PageDto<UserDto>> getAllUsersOrderByPersistDatePagination(@RequestParam(value = "page") Integer page,
+                                                                                    @RequestParam(value = "items", required = false,
+                                                                                            defaultValue = "10") Integer items) {
+        Map<String, Object> params = new HashMap<>();
+        params.put("currentPageNumber", page);
+        params.put("itemsOnPage", items);
 
+        PageDto<UserDto> pageDto = userDtoService.getPageDto("paginationUser", params);
+        return pageDto != null && !pageDto.getItems().isEmpty()
+                ? new ResponseEntity<>(pageDto, HttpStatus.OK)
+                : new ResponseEntity<>(HttpStatus.NOT_FOUND);
+    }
 }
