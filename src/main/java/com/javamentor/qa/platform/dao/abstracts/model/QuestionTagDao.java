fca9a2c5e123a8fc8c8df3c9639ddde3f74bdca3
RussevaYulia <xxxdd@bk.ru>
2021-12-23 17:11:44 +0300

    add init Question, Answer, Tag, QuestionTag, Reputation

--- src/main/java/com/javamentor/qa/platform/dao/abstracts/model/QuestionTagDao.java
+++
@@ -1,7 +0,0 @@
-package com.javamentor.qa.platform.dao.abstracts.model;
-
-import com.javamentor.qa.platform.models.entity.question.Question;
-import com.javamentor.qa.platform.models.entity.question.Tag;
-
-public interface QuestionTagDao extends ReadWriteDao<Question, Tag>{
-}
--- src/main/java/com/javamentor/qa/platform/service/abstracts/model/QuestionTagService.java
+++
@@ -1,7 +0,0 @@
-package com.javamentor.qa.platform.service.abstracts.model;
-
-import com.javamentor.qa.platform.models.entity.question.Question;
-import com.javamentor.qa.platform.models.entity.question.Tag;
-
-public interface QuestionTagService extends ReadWriteService<Question, Tag>{
-}
