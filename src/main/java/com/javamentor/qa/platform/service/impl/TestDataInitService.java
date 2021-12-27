f93cf0f41a46abca53cbc1c7c900899076ae0ae6
RussevaYulia <xxxdd@bk.ru>
2021-12-27 06:07:25 +0300

    add init Question, Answer, Tag, QuestionTag, Reputation

--- src/main/java/com/javamentor/qa/platform/service/impl/TestDataInitService.java
+++ src/main/java/com/javamentor/qa/platform/service/impl/TestDataInitService.java
@@ -1,14 +1,19 @@
 package com.javamentor.qa.platform.service.impl;
 
+import com.javamentor.qa.platform.models.entity.question.Question;
+import com.javamentor.qa.platform.models.entity.question.Tag;
+import com.javamentor.qa.platform.models.entity.question.answer.Answer;
 import com.javamentor.qa.platform.models.entity.user.Role;
 import com.javamentor.qa.platform.models.entity.user.User;
-import com.javamentor.qa.platform.service.abstracts.model.RoleService;
-import com.javamentor.qa.platform.service.abstracts.model.UserService;
+import com.javamentor.qa.platform.models.entity.user.reputation.Reputation;
+import com.javamentor.qa.platform.models.entity.user.reputation.ReputationType;
+import com.javamentor.qa.platform.service.abstracts.model.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Lazy;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
+import java.util.*;
 
 @Service
 public class TestDataInitService {
@@ -17,14 +22,31 @@ public class TestDataInitService {
 
     private UserService userService;
 
+    private AnswerService answerService;
+
+    private QuestionService questionService;
+
+    private ReputationService reputationService;
+
+    private TagService tagService;
+
 
     public TestDataInitService() {
     }
 
     @Autowired
-    public TestDataInitService(@Lazy RoleService roleService, @Lazy UserService userService) {
+    public TestDataInitService(@Lazy RoleService roleService,
+                               @Lazy UserService userService,
+                               @Lazy AnswerService answerService,
+                               @Lazy QuestionService questionService,
+                               @Lazy ReputationService reputationService,
+                               @Lazy TagService tagService) {
         this.roleService = roleService;
         this.userService = userService;
+        this.answerService = answerService;
+        this.questionService = questionService;
+        this.reputationService = reputationService;
+        this.tagService = tagService;
     }
 
     public void createRole() {
@@ -54,10 +76,114 @@ public class TestDataInitService {
         }
     }
 
-    public void init() {
-        createRole();
-        createUser(50);
+    public void createTag(int count) {
+        for (int i = 1; i <= count; i++) {
+            Tag tag = new Tag();
+            tag.setDescription("Tag Description " + i);
+            tag.setName("Tag " + i);
+            tag.setPersistDateTime(LocalDateTime.of(2021, 10, 01, 14, 05, 00));
+            tagService.persist(tag);
+        }
+    }
+
+    public void createQuestion(int count) {
+        //Question c 1 Tag`ом
+        Question question1 = new Question();
+        question1.setDescription("Question Description 0");
+        question1.setIsDeleted(false);
+        question1.setLastUpdateDateTime(LocalDateTime.of(2021, 10, 01, 18, 05, 00));
+        question1.setTitle("Question Title 0");
+        List<Tag> tagList1 = new ArrayList<>();
+        tagList1.add(tagService.getAll().get(0));
+        question1.setTags(tagList1);
+        question1.setUser(userService.getAll().get(1));
+        questionService.persist(question1);
+
+        //Question c 4 Tag`ами
+        Question question2 = new Question();
+        question2.setDescription("Question Description 1");
+        question2.setIsDeleted(false);
+        question2.setLastUpdateDateTime(LocalDateTime.of(2021, 10, 01, 18, 05, 00));
+        question2.setTitle("Question Title 1");
+        List<Tag> tagList2 = new ArrayList<>();
+        for (int k = 0; k < 4; k++) {
+            tagList2.add(tagService.getAll().get(k));
+        }
+        question2.setTags(tagList2);
+        question2.setUser(userService.getAll().get(1));
+        questionService.persist(question2);
+
+        //Question c рандомным кол-вом Tag`в
+        for (int i = 2; i <= count; i++) {
+            int random = (int) (Math.random() * 5);
+            Question question3 = new Question();
+            question3.setDescription("Question Description " + i);
+            question3.setIsDeleted(false);
+            question3.setLastUpdateDateTime(LocalDateTime.of(2021, 10, 01, 18, 05, 00));
+            question3.setTitle("Question Title " + i);
+
+            Set<Tag> tagSet = new HashSet<>();
+            for (int j = 0; j <= random; j++) {
+
+                tagSet.add(tagService.getAll().get((int) (Math.random() * 4)));
+            }
+            List<Tag> tagList = new ArrayList<>(tagSet);
+
+            question3.setTags(tagList);
+            question3.setUser(userService.getAll().get(i));
+            questionService.persist(question3);
+        }
     }
 
 
+    public void createAnswer() {
+        List<Question> questionList = questionService.getAll();
+
+        // У Question'a c id=1 нет answer
+        for (int i = 1; i < questionList.size(); i++) {
+            int random = (int) (Math.random() * 7);
+
+            for (int j = 1; j <= random; j++) {
+            Answer answer = new Answer();
+            answer.setDateAcceptTime(LocalDateTime.of(2021, 12, 01, 14, 05, 00));
+            answer.setHtmlBody("Answer Body " + j);
+            answer.setIsDeleted(false);
+            answer.setIsDeletedByModerator(false);
+            answer.setIsHelpful(true);
+            answer.setPersistDateTime(LocalDateTime.of(2021, 12, 01, 14, 05, 00));
+            answer.setUpdateDateTime(LocalDateTime.of(2021, 12, 01, 14, 05, 00));
+            answer.setUser(userService.getAll().get((int) (Math.random() * 50)));
+            answer.setQuestion(questionList.get(i));
+            answerService.persist(answer);
+            }
+        }
+    }
+
+    public void createReputation() {
+        List<User> userList = userService.getAll();
+        for (int i = 0; i < userList.size(); i++) {
+            Reputation reputation = new Reputation();
+            reputation.setCount((int) (Math.random() * 50));
+            reputation.setPersistDate(LocalDateTime.of(2021, 12, 01, 14, 05, 00));
+            if (i % 2 == 0) {
+                reputation.setAuthor(userService.getAll().get(i));
+                reputation.setType(ReputationType.Question);
+                reputation.setQuestion(questionService.getAll().get(i));
+            } else {
+                reputation.setAuthor(userService.getAll().get(i));
+                reputation.setType(ReputationType.Answer);
+                reputation.setAnswer(answerService.getAll().get(i));
+            }
+            reputationService.persist(reputation);
+        }
+    }
+
+    public void init() {
+        createRole();
+        createUser(50);
+        createTag(4);
+        createQuestion(50);
+        createAnswer();
+        createReputation();
+    }
 }
