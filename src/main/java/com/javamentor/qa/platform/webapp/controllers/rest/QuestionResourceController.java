715b1758e0242e868b3defb58d0ef9a9524878a3
Али Велиев <c.i.klop@ya.ru>
2021-12-28 17:12:21 +0300

    #3 Done!

--- src/main/java/com/javamentor/qa/platform/webapp/controllers/rest/QuestionResourceController.java
+++ src/main/java/com/javamentor/qa/platform/webapp/controllers/rest/QuestionResourceController.java
@@ -1,16 +1,22 @@
 package com.javamentor.qa.platform.webapp.controllers.rest;
 
+import com.javamentor.qa.platform.models.dto.QuestionCreateDto;
+import com.javamentor.qa.platform.models.entity.question.Question;
+import com.javamentor.qa.platform.models.entity.user.User;
 import com.javamentor.qa.platform.service.abstracts.dto.QuestionDtoService;
 import com.javamentor.qa.platform.service.abstracts.model.QuestionService;
+import com.javamentor.qa.platform.webapp.converters.QuestionConverter;
+import com.javamentor.qa.platform.webapp.converters.TagConverter;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
 
 /**
  * @author Ali Veliev 10.12.2021
@@ -18,17 +24,20 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/user/question")
-@Api("Rest Contoller to get a Question by ID")
+@Api("Rest Contoller for Question")
 public class QuestionResourceController {
-
 
     @Autowired
     private QuestionDtoService questionDtoService;
 
     private final QuestionService questionService;
+    private final QuestionConverter questionConverter;
+    private final TagConverter tagConverter;
 
-    public QuestionResourceController(QuestionService questionService) {
+    public QuestionResourceController(QuestionService questionService, QuestionConverter questionConverter, TagConverter tagConverter) {
         this.questionService = questionService;
+        this.questionConverter = questionConverter;
+        this.tagConverter = tagConverter;
     }
 
     @GetMapping("/count")
@@ -47,5 +56,25 @@ public class QuestionResourceController {
             new ResponseEntity<>("Wrong Question ID!", HttpStatus.NOT_FOUND) :
                 new ResponseEntity<>(questionDtoService.getQuestionById(id), HttpStatus.OK);
 
+    }
+
+    @PostMapping("/")
+    @ApiOperation("API создания вопроса. Получает объект QuestionCreateDto. " +
+            "Возвращет объект QuestionDto. Поля Title, Description, Tag должны быть заполнены." +
+            "Если хотя бы одно поле не заполнено возвращается HttpStatus.BAD_REQUEST." +
+            "Проверяет есть ли присланный Tag в базе. Если нет - создает.")
+    public ResponseEntity<?> createQuestion(@Valid @RequestBody QuestionCreateDto questionCreateDto) {
+
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+
+        Question question = new Question();
+        question.setTitle(questionCreateDto.getTitle());
+        question.setUser((User) authentication.getPrincipal());
+        question.setDescription(questionCreateDto.getDescription());
+        question.setTags(tagConverter.listTagDtoToListTag(questionCreateDto.getTags()));
+
+        questionService.persist(question);
+
+        return new ResponseEntity<>(questionConverter.questionToQuestionDto(question), HttpStatus.OK);
     }
 }
