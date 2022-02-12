73a73255e665e5a21db5f31fa01857a2ef7cacfa
IYudashkin <yudashkin.work@gmail.com>
2022-02-12 18:23:03 +0400

    Перенес логику удаления из ДТО в ДАО

--- src/main/java/com/javamentor/qa/platform/dao/impl/model/AnswerDaoImpl.java
+++ src/main/java/com/javamentor/qa/platform/dao/impl/model/AnswerDaoImpl.java
@@ -4,6 +4,21 @@ import com.javamentor.qa.platform.dao.abstracts.model.AnswerDao;
 import com.javamentor.qa.platform.models.entity.question.answer.Answer;
 import org.springframework.stereotype.Repository;
 
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
 @Repository
 public class AnswerDaoImpl extends ReadWriteDaoImpl<Answer,Long> implements AnswerDao {
+
+    @PersistenceContext
+    private EntityManager entityManager;
+
+    @Override
+    public void deleteById(Long id) {
+        String query = "UPDATE Answer SET isDeleted = true where id = :id";
+        entityManager.createQuery(query)
+                .setParameter("id", id)
+                .executeUpdate();
+    }
+
 }
