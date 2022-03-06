ee33c467ce8f87c9b6d31640786eb928482ac0a0
Michael <dizel882@gmail.com>
2022-03-06 12:52:28 +0300

    добавил очистку токена из куки после возникновения 401, 403 и 491 ошибок

--- src/main/resources/static/js/authorization_check.js
+++ src/main/resources/static/js/authorization_check.js
@@ -1,17 +1,24 @@
-$(document).ready(function() {
+$(document).ready(function () {
     checkAuthorization();
 })
 
 function checkAuthorization() {
     const response = fetch("http://localhost:8091/api/user/check_auth/", {
         method: "GET",
-        headers: { "Accept": "application/json", "Content-Type": "application/json" },
+        headers: {"Accept": "application/json", "Content-Type": "application/json"},
     });
 
     if (response.status === 401) {
+        clearToken();
         window.location.href = '/login';
-    } else if (response.status === 403) {
+    } else if (response.status === 403 || response.status === 491) {
+        clearToken();
         window.location.href = '/access_denied';
     }
 }
+
+function clearToken() {
+    document.cookie = 'token=;expires=Thu, 01 Jan 1970 00:00:01 GMT;';
+}
+
 
