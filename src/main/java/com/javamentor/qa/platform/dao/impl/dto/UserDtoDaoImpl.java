12fe706dd5f32be02949dcf11253a4aa0ea3d1e4
Michael <dizel882@gmail.com>
2022-03-13 10:24:11 +0300

    Добавил в UserDto поле в котором будет топ 3 тегов ответов пользователя

--- src/main/java/com/javamentor/qa/platform/dao/impl/dto/UserDtoDaoImpl.java
+++ src/main/java/com/javamentor/qa/platform/dao/impl/dto/UserDtoDaoImpl.java
@@ -1,11 +1,14 @@
 package com.javamentor.qa.platform.dao.impl.dto;
 
 import com.javamentor.qa.platform.dao.abstracts.dto.UserDtoDao;
+import com.javamentor.qa.platform.models.dto.TagDto;
 import com.javamentor.qa.platform.models.dto.UserDto;
 import org.springframework.stereotype.Repository;
 
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Optional;
 
 /**
@@ -19,11 +22,31 @@ public class UserDtoDaoImpl implements UserDtoDao {
     private EntityManager entityManager;
 
     @Override
-    public Optional<UserDto> getUserById(Long id){
+    public Optional<UserDto> getUserById(Long id) {
 
-        return entityManager.createQuery("select new com.javamentor.qa.platform.models.dto.UserDto(rep.id, rep.author.email, rep.author.fullName,rep.author.imageLink,rep.author.city,rep.count)" +
-                        " from Reputation rep where rep.author.id =: id", UserDto.class)
-                .setParameter("id", id).getResultStream().findAny();
+        List<TagDto> topTags = entityManager.createQuery(
+                        "select new com.javamentor.qa.platform.models.dto.TagDto(" +
+                                "t.id, " +
+                                "t.name, " +
+                                "t.description) " +
+                                "from Tag t join t.questions tq join tq.answers qa where qa.user.id =: id group by t.id order by count(t.id)"
+                        , TagDto.class)
+                .setParameter("id", id)
+                .setMaxResults(3)
+                .getResultList();
+        UserDto userDto = entityManager.createQuery(
+                        "select new com.javamentor.qa.platform.models.dto.UserDto (" +
+                                "rep.id, " +
+                                "rep.author.email, " +
+                                "rep.author.fullName, " +
+                                "rep.author.imageLink, " +
+                                "rep.author.city, " +
+                                "rep.count) " +
+                                "from Reputation rep where rep.author.id =: id "
+                        , UserDto.class)
+                .setParameter("id", id)
+                .getSingleResult();
 
+        return Optional.of(new UserDto(userDto.getId(), userDto.getEmail(), userDto.getFullName(), userDto.getLinkImage(), userDto.getCity(), userDto.getReputation(), topTags));
     }
 }
