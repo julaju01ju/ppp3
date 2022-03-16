b1dde771947eb0073a614ea96c3d9bc09a35087b
Michael <dizel882@gmail.com>
2022-03-16 22:12:57 +0300

    Дообавил элементов в карточку вопроса

--- src/main/resources/static/js/example/questions.js
+++ src/main/resources/static/js/example/questions.js
@@ -46,27 +46,86 @@ function fillCard(elementItems) {
                 .append($('<div class="d-flex flex-column">')
                     .append($('<div class="question-card-body">')
                         .attr("style", "margin-left: 2rem; overflow: hidden; height: 5rem; margin-bottom: .7rem")
-                        .append($('<a href="#" class="question-heading">')
-                            .attr("style", "text-decoration: none").text(item.title))
+                        .append($('<h3 class="question-card-title">')
+                            .attr("style", "font-weight: 400;word-break: break-word !important;overflow-wrap: break-word !important;display: block;font-size: 1rem;line-height: 1.3;margin: 0 0 1em;")
+                            .append($('<a href="#" class="question-heading">')
+                                .attr("style", "text-decoration: none").text(item.title)))
                         .append($('<div class="question-content">')
                             .attr("style", "font-size: .8rem; margin-top: 1rem")
                             .append($('<p>').text(item.description))))
                     .append($('<div class="tag-list" id="tag-list">')
                         .attr("style", "height: 2rem")
-                        .append($('<ul class="d-flex">')
-                            .attr("style", "list-style: none")
+                        .append($('<div class="d-flex">')
+                            .attr("style", "line-height: 18px;float: left;margin: 0;padding: 0;border: 0;font: inherit;vertical-align: baseline;")
                             .append(
                                 $.map(item.listTagDto, function (itemTag) {
-                                        return $('<li class="list-item">')
-                                            .append('<a href="#">')
-                                            .attr("style", "margin-left: 1rem; font-size: .7rem; background-color: lightblue; color: blue; padding: .3rem .5rem; border-radius: .2rem; text-decoration: none")
+                                        return $('<a class="list-item" href="#">')
+                                            .attr("style", "margin-left: 1rem; font-size: 12px; background-color: lightblue; color: blue; padding: .3rem .5rem;border-width: 1px;border-style: solid; border-radius: .2rem; text-decoration: none;display: inline-block;white-space: nowrap;text-align: center;line-height: 1;")
                                             .text(itemTag.name)
                                     }
                                 ))
-                        ))
-                )
+                            .append($('<div class="user-card user-car_minimal">')
+                                .attr("style", "display: flex; align-items: center; gap: 4px; margin: 0;padding: 0;border: 0;font: inherit;vertical-align: baseline; flex-wrap: wrap; margin-left: auto; justify-content: flex-end;")
+                                .append($('<a href="#" class="avatar avatar_16 user-card-avatar">')
+                                    .attr("style", "display: inline-block; position: relative; width: 16px; height: 16px; border-radius: 3px;margin: 0;padding: 0;border: 0;font: inherit; background-color: hsl(0,0%,100%); background-repeat: no-repeat; background-size: 100%;align-items: center; vertical-align: bottom;")
+                                    .append($('<div class="gravatar-wrapper-16">')
+                                        .attr("style", "overflow: hidden;align-items: center; margin: 0;padding: 0;border: 0;font: inherit;vertical-align: baseline;")
+                                        .append($('<img src="https://www.gravatar.com/avatar/9881fc9963d4fdff1dd454becd37a250?s=32&amp;d=identicon&amp;r=PG&amp;f=1" alt="" width="16" ,="" height="16" class="avatar--image">')
+                                            .attr("style", "width: 16px; height: 16px;border-radius: 3px; display: block;margin: 0 auto;box-sizing: inherit;padding: 0;border: 0;font: inherit;vertical-align: baseline;")
+                                        )))
+                                .append($('<div class="user-card--info">')
+                                    .attr("style", "flex-direction: row;align-items: center;display: flex;gap: 4px;")
+                                    .append($('<div class="user-card--link d-flex gs4">')
+                                        .attr("style", "margin: -2px;display: flex !important;white-space: nowrap;min-width: 0;font-size: 12px;align-items: center;flex-wrap: wrap;overflow-wrap: break-word;")
+                                        .append($('<a href="#" class="flex--item">')
+                                            .attr("style", "margin: 2px;text-decoration: none;")
+                                            .text(item.authorName))
+                                        .append($('<ul class="user-card--awards">')
+                                            .attr("style", "list-style: none;margin: 0;padding: 0;align-items: center;display: flex;gap: 6px;")
+                                            .append($('<li class="user-card--rep">')
+                                                .attr("style", "text-align: -webkit-match-parent;")
+                                                .append($('<span class="todo-no-class-here">')
+                                                    .attr("style", "direction: ltr;unicode-bidi: isolate;font-size: 100%;font-weight: bold;")
+                                                    .attr("title", "reputation score ")
+                                                    .attr("dir", "ltr")
+                                                    .text(item.authorReputation)
+                                                ))
+                                        )))
+                                .append($('<time class="user-card--time">')
+                                    .attr("style", "white-space: nowrap;font-size: 12px;")
+                                    .text("asked ")
+                                    .append($('<span class="relativetime">')
+                                        .attr("title", item.persistDateTime).text(timeDifference(item.persistDateTime)))
+                                )))))
 
         $('#questions_list').prepend(newElement);
 
     });
+}
+
+function timeDifference(previous) {
+
+    let msPerMinute = 60 * 1000;
+    let msPerHour = msPerMinute * 60;
+    let msPerDay = msPerHour * 24;
+    let msPerMonth = msPerDay * 30;
+    let msPerYear = msPerDay * 365;
+
+    previous = new Date(previous);
+    let current = new Date();
+    let elapsed = current.getTime() - previous.getTime();
+
+    if (elapsed < msPerMinute) {
+        return Math.round(elapsed / 1000) + ' seconds ago';
+    } else if (elapsed < msPerHour) {
+        return Math.round(elapsed / msPerMinute) + ' minutes ago';
+    } else if (elapsed < msPerDay) {
+        return Math.round(elapsed / msPerHour) + ' hours ago';
+    } else if (elapsed < msPerMonth) {
+        return Math.round(elapsed / msPerDay) + ' days ago';
+    } else if (elapsed < msPerYear) {
+        return Math.round(elapsed / msPerMonth) + ' months ago';
+    } else {
+        return Math.round(elapsed / msPerYear) + ' years ago';
+    }
 }
