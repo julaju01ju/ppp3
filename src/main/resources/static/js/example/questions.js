820efdf7668a3c4965025290eaf378bee9e6e3b4
Michael <dizel882@gmail.com>
2022-03-16 23:38:45 +0300

    Добавил отображение общего количетва вопрорсов

--- src/main/resources/static/js/example/questions.js
+++ src/main/resources/static/js/example/questions.js
@@ -1,5 +1,6 @@
 $(document).ready(async function () {
 
+    await getAllQuestionsCount();
     await pagination(1, 50, getMostPopularQuestion);
 })
 
@@ -16,7 +17,27 @@ function getMostPopularQuestion(page, itemsOnPage) {
         });
 }
 
+function getAllQuestionsCount() {
+    return fetch(`/api/user/question/count`,
+        {
+            method: 'GET',
+            headers: {
+                'Authorization': 'Bearer ' + getCookie('token'),
+                'Accept': 'application/json', 'Content-Type': 'application/json'
+            },
+        }).then(response => {
+        response.json().then(questionsCount => {
+
+            const questionsCountDecl = [" вопрос", " вопроса", " вопросов"];
+            let decl = declOfNum(questionsCount, questionsCountDecl);
+            $(".counter-questions").text(questionsCount + decl);
+
+        })
+    });
+}
+
 function fillCard(elementItems) {
+
     $('#questions_list').empty();
 
     elementItems.forEach(function (item) {
@@ -128,4 +149,19 @@ function timeDifference(previous) {
     } else {
         return Math.round(elapsed / msPerYear) + ' years ago';
     }
+}
+
+function declOfNum(n, text_forms) {
+    n = Math.abs(n) % 100;
+    let n1 = n % 10;
+    if (n > 10 && n < 20) {
+        return text_forms[2];
+    }
+    if (n1 > 1 && n1 < 5) {
+        return text_forms[1];
+    }
+    if (n1 === 1) {
+        return text_forms[0];
+    }
+    return text_forms[2];
 }
