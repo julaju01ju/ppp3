e8d47443fa798376e1c58cac44203d727e5820f1
Mike15426378 / <Ravaga228>
2022-04-15 10:26:28 +0300

    внес правки, кроме проблем с кейсами в тесте, хз что имеется ввиду

--- src/main/java/com/javamentor/qa/platform/service/abstracts/dto/CommentDtoService.java
+++ src/main/java/com/javamentor/qa/platform/service/abstracts/dto/CommentDtoService.java
@@ -3,5 +3,5 @@ package com.javamentor.qa.platform.service.abstracts.dto;
 import com.javamentor.qa.platform.models.dto.CommentDto;
 
 public interface CommentDtoService {
-    CommentDto checkMyCommentDtoByQuestionId(Long id);
+    CommentDto getLastAddedCommentDtoByQuestionId(Long id);
 }
