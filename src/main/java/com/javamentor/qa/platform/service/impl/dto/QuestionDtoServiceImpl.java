aba1791aebf096e18e382a83d32d6ab0f1764cb8
Mike15426378 / <Ravaga228>
2022-04-23 10:50:43 +0300

    v1

--- src/main/java/com/javamentor/qa/platform/service/impl/dto/QuestionDtoServiceImpl.java
+++ src/main/java/com/javamentor/qa/platform/service/impl/dto/QuestionDtoServiceImpl.java
@@ -3,12 +3,15 @@ package com.javamentor.qa.platform.service.impl.dto;
 import com.javamentor.qa.platform.dao.abstracts.dto.CommentDtoDao;
 import com.javamentor.qa.platform.dao.abstracts.dto.QuestionDtoDao;
 import com.javamentor.qa.platform.dao.abstracts.dto.TagDtoDao;
+import com.javamentor.qa.platform.dao.abstracts.model.BookMarksDao;
 import com.javamentor.qa.platform.models.dto.PageDto;
 import com.javamentor.qa.platform.models.dto.QuestionDto;
 import com.javamentor.qa.platform.models.dto.QuestionViewDto;
 import com.javamentor.qa.platform.models.dto.TagDto;
+import com.javamentor.qa.platform.models.entity.user.User;
 import com.javamentor.qa.platform.service.abstracts.dto.QuestionDtoService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -26,12 +29,14 @@ public class QuestionDtoServiceImpl extends PageDtoServiceImpl<QuestionViewDto> 
     private final QuestionDtoDao questionDtoDao;
     private final CommentDtoDao commentDtoDao;
     private final TagDtoDao tagDtoDao;
+    private final BookMarksDao bookMarksDao;
 
     @Autowired
-    public QuestionDtoServiceImpl(QuestionDtoDao questionDtoDao, CommentDtoDao commentDtoDao, TagDtoDao tagDtoDao) {
+    public QuestionDtoServiceImpl(QuestionDtoDao questionDtoDao, CommentDtoDao commentDtoDao, TagDtoDao tagDtoDao, BookMarksDao bookMarksDao) {
         this.questionDtoDao = questionDtoDao;
         this.commentDtoDao = commentDtoDao;
         this.tagDtoDao = tagDtoDao;
+        this.bookMarksDao = bookMarksDao;
     }
 
     @Override
@@ -39,6 +44,8 @@ public class QuestionDtoServiceImpl extends PageDtoServiceImpl<QuestionViewDto> 
         Optional<QuestionDto> questionDto = questionDtoDao.getQuestionById(id);
         questionDto.ifPresent(dto -> dto.setListCommentDto(commentDtoDao.getCommentDtosByQuestionId(id)));
         questionDto.ifPresent(dto -> dto.setListTagDto(tagDtoDao.getTagsByQuestionId(id)));
+        questionDto.ifPresent(dto -> dto.setIsUserBookmark(bookMarksDao.isQuestionAlreadyExistOnUserBookmarks(
+                ((User) SecurityContextHolder.getContext().getAuthentication().getPrincipal()).getId(), id)));
         return questionDto;
     }
 
