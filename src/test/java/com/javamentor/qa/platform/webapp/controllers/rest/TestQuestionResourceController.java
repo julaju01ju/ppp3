427879447161ed6c95a2b4373953117e18ff7aa8
IvanSid174 <ivansidorov2202@mail.ru>
2022-05-11 23:31:08 +0500

    getQuestionsWithTrackedAndIgnoredTagsInParams добавлено проверка поля isUserBookMarks

--- src/test/java/com/javamentor/qa/platform/webapp/controllers/rest/TestQuestionResourceController.java
+++ src/test/java/com/javamentor/qa/platform/webapp/controllers/rest/TestQuestionResourceController.java
@@ -772,7 +772,8 @@ public class TestQuestionResourceController extends AbstractControllerTest {
             "dataset/QuestionResourceController/questions.yml",
             "dataset/QuestionResourceController/question_has_tag.yml",
             "dataset/QuestionResourceController/reputations.yml",
-            "dataset/QuestionResourceController/roles.yml"
+            "dataset/QuestionResourceController/roles.yml",
+
     },
             disableConstraints = true, cleanBefore = true)
     public void getQuestionsWithTrackedAndIgnoredTagsInParams() throws Exception {
@@ -786,9 +787,14 @@ public class TestQuestionResourceController extends AbstractControllerTest {
                 .andExpect(jsonPath("$.totalPageCount").value(1))
                 .andExpect(jsonPath("$.itemsOnPage").value(3))
                 .andExpect(jsonPath("$.items[0].listTagDto[0].id").value(101))
+                .andExpect(jsonPath("$.items[0].isUserBookMarks").value("false"))
                 .andExpect(jsonPath("$.items[1].listTagDto[0].id").value(101))
+                .andExpect(jsonPath("$.items[1].isUserBookMarks").value("false"))
                 .andExpect(jsonPath("$.items[1].listTagDto[1].id").value(102))
-                .andExpect(jsonPath("$.items[2].listTagDto[0].id").value(104));
+                .andExpect(jsonPath("$.items[1].isUserBookMarks").value("false"))
+                .andExpect(jsonPath("$.items[2].listTagDto[0].id").value(104))
+                .andExpect(jsonPath("$.items[2].isUserBookMarks").value("false"));
+
     }
 
     @Test
