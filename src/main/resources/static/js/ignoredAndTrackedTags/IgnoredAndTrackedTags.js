b98600982755af36f44435300fcc84bcd63b2809
Michael Saratovskiy <bigmsx@gmail.com>
2022-05-24 14:51:33 +0300

    add ignored and tracked tags int the main page

--- src/main/resources/static/js/ignoredAndTrackedTags/IgnoredAndTrackedTags.js
+++ src/main/resources/static/js/ignoredAndTrackedTags/IgnoredAndTrackedTags.js
@@ -3,30 +3,74 @@ header.append('Content-Type', 'application/json');
 header.append('Authorization', 'Bearer ' + getCookie('token'));
 const trackedTags = document.querySelector('#trackedTags');
 const ignoredTags = document.querySelector('#ignoredTags');
+const searchTagTracked = document.querySelector('#searchTrackedTag')
+const searchTagIgnored = document.querySelector('#searchIgnoreTag')
+
 const trackUrl = '/api/user/tag/tracked';
 const ignoreUrl = '/api/user/tag/ignored';
 
-let renderTrackedTags = (element) => {
+////////////////////////////
+// Методы для вывода данных
+
+function renderTags(element, inner) {
     let output = '';
     element.forEach(tag => {
         output += `
                     <div><a href="#" style="  background-color:lightblue; font-size: .8rem;
                     color: darkblue; padding: .3rem .5rem; border-radius: .2rem;
-                    text-decoration: none" id="selectTagsFromTrackedTags">${tag.name}</a></div>`;
+                    text-decoration: none" >${tag.name}</a></div>`;
     });
-    trackedTags.innerHTML = output;
+    inner.innerHTML = output;
 }
 
-let renderIgnoredTags = (element) => {
+function renderSearch(element, inner) {
     let output = '';
-    element.forEach(tag => {
-        output += `
-                    <div><a href="#" style="  background-color:lightblue; font-size: .8rem;
-                    color: darkblue; padding: .3rem .5rem; border-radius: .2rem;
-                    text-decoration: none">${tag.name}</a></div>`;
-    });
-    ignoredTags.innerHTML = output;
+        element.forEach(tag => {
+            output += `
+                     <option value="${tag.id}" >${tag.name}</option>`;
+        });
+        inner.innerHTML = output;
 }
+
+//////////////////////
+// Для задержки вывода
+
+document.getElementById('trackedTagId').oninput = debounce(getLatterTracked, 500);
+document.getElementById('ignoredTagId').oninput = debounce(getLatterIgnored, 500);
+
+function debounce(call, timeout) {
+    return function perform(...args) {
+        let previousCall = this.lastCall;
+        this.lastCall = Date.now();
+        if (previousCall && this.lastCall - previousCall <= timeout) {
+            clearTimeout(this.lastCallTimer);
+        }
+        this.lastCallTimer = setTimeout(() => call(...args), timeout);
+    }
+}
+
+function getLatterTracked() {
+    let param = document.getElementById('trackedTagId').value
+    fetch(`/api/user/tag/latter?searchString=${param}`, {
+        method: 'GET',
+        headers: header
+    }).then(res => res.json()).then(data => {
+        renderSearch(data, searchTagTracked);
+    })
+}
+
+function getLatterIgnored() {
+    let param = document.getElementById('ignoredTagId').value
+    fetch(`/api/user/tag/latter?searchString=${param}`, {
+        method: 'GET',
+        headers: header
+    }).then(res => res.json()).then(data => {
+        renderSearch(data, searchTagIgnored);
+    })
+}
+
+//////////////////////////////////
+// Запросы на получения всех тэгов
 
 function getTrackedTags() {
     return fetch(trackUrl, {
@@ -34,7 +78,7 @@ function getTrackedTags() {
         headers: header
     })
         .then(res => res.json())
-        .then(data => renderTrackedTags(data))
+        .then(data => renderTags(data, trackedTags))
 }
 
 function getIgnoredTags() {
@@ -43,8 +87,11 @@ function getIgnoredTags() {
         headers: header
     })
         .then(res => res.json())
-        .then(data => renderIgnoredTags(data))
+        .then(data => renderTags(data, ignoredTags))
 }
+
+///////////////////////////////////////
+// Для вывода поиска по клику на кнопку
 
 function showAdd(id) {
     let div = document.getElementById(id);
@@ -57,16 +104,21 @@ function showAdd(id) {
 let checkIdTrackedTagsIfExists = "";
 let checkIdIgnoredTagsIfExists = "";
 
-function clearInput(){
+/////////////////////
+// Очистка поля ввода
+
+function clearInput() {
     let clearInputTr = document.getElementById('trackedTagId').value = "";
     let clearInputIgn = document.getElementById('ignoredTagId').value = "";
-
 }
+
+//////////////////////////////
+// Запросы на добавление тэгов
 
 function addIgnoredTag() {
     let id = document.getElementById('ignoredTagId').value
-    if (checkIdIgnoredTagsIfExists === id){
-        console.log('Tэг с id: ' + id + ' уже добавлен')
+    if (checkIdIgnoredTagsIfExists === id) {
+        console.log('Поле add tag не заполнено или метка уже добавлена')
         return clearInput();
     } else {
         checkIdIgnoredTagsIfExists = id;
@@ -82,17 +134,18 @@ function addIgnoredTag() {
 
 function addTrackedTag() {
     let id = document.getElementById('trackedTagId').value
-    if (checkIdTrackedTagsIfExists === id){
-        console.log('Tэг с id: ' + id + ' уже добавлен')
+    console.log(id)
+    if (checkIdTrackedTagsIfExists === id) {
+        console.log('Поле add tag не заполнено или метка уже добавлена')
         return clearInput();
     } else {
         checkIdTrackedTagsIfExists = id;
-    fetch(`/api/user/tag/${id}/tracked`, {
-        method: 'POST',
-        headers: header
-    })
-        .then(res => res.json())
-        .then(data => getTrackedTags(data));
+        fetch(`/api/user/tag/${id}/tracked`, {
+            method: 'POST',
+            headers: header
+        })
+            .then(res => res.json())
+            .then(data => getTrackedTags(data));
         clearInput();
     }
 }
@@ -101,6 +154,8 @@ function showTags() {
     getTrackedTags();
     getIgnoredTags();
 }
+
+
 
 
 
