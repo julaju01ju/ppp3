a9395689b49d546e409838b974adcf33c9b8669a
FinallRareStaff <Jeymester@gmail.com>
2022-06-15 17:31:40 +0300

    fix pagination's select

--- src/main/java/com/javamentor/qa/platform/dao/impl/dto/pagination/PaginationGroupChatMessages.java
+++ src/main/java/com/javamentor/qa/platform/dao/impl/dto/pagination/PaginationGroupChatMessages.java
@@ -22,32 +22,32 @@ public class PaginationGroupChatMessages implements PageDtoDao<MessageViewDto> {
         int page = (int) params.get("currentPageNumber");
         int itemsOnPage = (int) params.get("itemsOnPage");
 
-        return entityManager.createNativeQuery(
-                "select user_entity.id as user_entity_ud, " +
-                        "user_entity.full_name, " +
-                        "user_entity.image_link, " +
-                        "message.id, " +
-                        "message.message, " +
-                        "message.persist_date " +
-                        "from message join user_entity on message.user_sender_id = user_entity.id " +
-                        "join chat on message.chat_id = chat.id " +
-                        "where chat.chat_type = 1 " +
-                        "order by message.persist_date desc")
+        return entityManager.createQuery(
+                        "select new com.javamentor.qa.platform.models.dto.MessageViewDto" +
+                                "(m.id, " +
+                                "m.message, " +
+                                "u.fullName, " +
+                                "u.id, " +
+                                "u.imageLink, " +
+                                "m.persistDate) " +
+                                "from Message m join User u on m.userSender.id = u.id " +
+                                "join Chat c on m.chat.id = c.id " +
+                                "where c.chatType = 1 " +
+                                "order by m.persistDate desc"
+                        , MessageViewDto.class)
                 .setFirstResult((page - 1) * itemsOnPage)
                 .setMaxResults(itemsOnPage)
-                .unwrap(org.hibernate.query.Query.class)
-                .setResultTransformer(new MessageViewDtoResultTransformer())
                 .getResultList();
     }
 
     @Override
     public int getTotalResultCount(Map<String, Object> params) {
         return ((BigInteger) entityManager.createNativeQuery(
-                "select count (message.id) " +
-                        "from message " +
-                        "inner join user_entity on message.user_sender_id = user_entity.id " +
-                        "inner join chat on message.chat_id = chat.id " +
-                        "where chat.chat_type = 1")
+                        "select count (message.id) " +
+                                "from message " +
+                                "inner join user_entity on message.user_sender_id = user_entity.id " +
+                                "inner join chat on message.chat_id = chat.id " +
+                                "where chat.chat_type = 1")
                 .getSingleResult()).intValue();
     }
 }
