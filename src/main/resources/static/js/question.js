d9c8aab5b84231d959a79cab431edae74a50e4a4
NomadKim <99548558+NomadKim@users.noreply.github.com>
2022-06-17 23:07:43 +0900

    requestToQuestionId_view

--- src/main/resources/static/js/question.js
+++ src/main/resources/static/js/question.js
@@ -24,13 +24,3 @@ form.addEventListener("submit", async e => {
 
     }
 });
-
-
-async function requestToquestionId_view(){
-    return await fetch("/api/user" + window.location.pathname + "/view", {
-            method: "POST",
-            headers: {
-                'Authorization': 'Bearer ' + token}
-        }
-    ).then(responce => responce.text());
-}
