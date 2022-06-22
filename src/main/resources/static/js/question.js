dc4e77231a60b60900cdfd0e15c7463889bb92dd
Eddi777 <Gjhnfkbot1>
2022-06-22 22:17:28 +0300

    Answers comment added commit

--- src/main/resources/static/js/question.js
+++ src/main/resources/static/js/question.js
@@ -1,10 +1,10 @@
 const questionId = document.location.href.split("/").slice(-1)[0].replace(/\D/g,'');
 const sendAnswer = document.querySelector('#handleQuestion');
-const addCommentToQuestion = document.querySelector('#question-add-comment-button');
 
 $(document).ready(async function () {
 
     await questionFilling();
+    fillQuestionComments();
     await answersFilling();
 })
 
@@ -98,64 +98,87 @@ function answersFilling() {
                     } else {
                         document.querySelector('#count-answers').textContent =
                             data.length + declOfNum(data.length, [" ответ", " ответа", " ответов"]);
+                        let mainBlock = document.querySelector('#answer-row');
+
                         data.forEach(function (item) {
-                            text +=
-                '                <div class="flex-row">\n' +
-                '                    <div class="col">\n' +
-                '                        <div class="row">\n' +
-                '                            <div class="col-1 text-center" id="voteplace-1">\n' +
-                '                                <ul class="list-unstyled">\n' +
-                '                                    <li>' +
-                '                                       <a id="voteUp-1" href="#">' +
-                '                                           <i class="fas fa-arrow-circle-up" style="color: rgb(121,135,155);"></i>' +
-                '                                       </a>' +
-                '                                    </li>\n' +
-                '                                    <li id="countVote-1">' + votesNumber(item.countValuable) + '</li>\n' +
-                '                                    <li id="voteDown-1">' +
-                '                                       <a href="#">' +
-                '                                           <i class="fa fa-arrow-circle-down" style="color: rgb(121,135,155);"></i>' +
-                '                                       </a>' +
-                '                                    </li>\n' +
-                '                                    <li>' +
-                '                                       <a href="#">' +
-                '                                           <i class="fa fa-rotate-right" style="color: rgb(121,135,155);"></i>' +
-                '                                       </a>' +
-                '                                    </li>\n' +
-                '                                </ul>\n' +
-                '                            </div>\n' +
-                '                            <div class="col" id="questionplace-1">\n' +
-                '                                <div id="answer-body">\n' +
-                '                                    <p>' + item.body + '<br></p>\n' +
-                '                                </div>\n' +
-                '                                <div id="users-info-1">\n' +
-                '                                    <div class="row">\n' +
-                '                                        <div class="col">\n' +
-                '                                            <div style="display: inline;">\n' +
-                '                                                <div><a href="#"><small class="text-secondary">Править</small></a></div>\n' +
-                '                                            </div>\n' +
-                '                                        </div>\n' +
-                '                                        <div class="col"></div>\n' +
-                '                                        <div class="col" style="background-color:lightblue; padding: .2rem .5rem; border-radius: .2rem; text-decoration: none">\n' +
-                '                                            <div><small>Ответ: ' + timeDifference(item.persistDate) + '</small></div>\n' +
-                '                                            <div></div>\n' +
-                '                                            <ul class="list-unstyled">\n' +
-                '                                                <li><i class="fa fa-star"></i><small>' + item.nickName + '</small></li>\n' +
-                '                                            </ul>\n' +
-                '                                        </div>\n' +
-                '                                    </div>\n' +
-                '                                </div>\n' +
-                '                                <div><a href="#"><small class="text-secondary">Добавить комментарий</small></a></div>\n' +
-                '                            </div>\n' +
-                '                        </div>\n' +
-                '                    </div>' +
-                '                    <hr>' +
-                '                </div>';
-                            });
-                        }
-                        document.querySelector('#answer-row').innerHTML = text;
-                    })
+                            let block = document.createElement("div");
+                            block.innerHTML = getAnswerHTMLForQuestion(item);
+                            mainBlock.appendChild(block);
+                            fillCommentsToAnswer("#anwserComments-" + item.id);
+                        })
+                    }
+                })
             }
-    });
+        })
+}
+
+function getAnswerHTMLForQuestion(item) {
+    let answerCommentsId = "anwserComments-" + item.id;
+    let buttonCommentsId = "buttonAnswerComments-" + item.id;
+
+    return '                <div class="flex-row">\n' +
+        '                    <div class="col">\n' +
+        '                        <div class="row">\n' +
+        '                            <div class="col-1 text-center" id="voteplace-1">\n' +
+        '                                <ul class="list-unstyled">\n' +
+        '                                    <li>' +
+        '                                       <a id="voteUp-1" href="#">' +
+        '                                           <i class="fas fa-arrow-circle-up" style="color: rgb(121,135,155);"></i>' +
+        '                                       </a>' +
+        '                                    </li>\n' +
+        '                                    <li id="countVote-1">' + votesNumber(item.countValuable) + '</li>\n' +
+        '                                    <li id="voteDown-1">' +
+        '                                       <a href="#">' +
+        '                                           <i class="fa fa-arrow-circle-down" style="color: rgb(121,135,155);"></i>' +
+        '                                       </a>' +
+        '                                    </li>\n' +
+        '                                    <li>' +
+        '                                       <a href="#">' +
+        '                                           <i class="fa fa-rotate-right" style="color: rgb(121,135,155);"></i>' +
+        '                                       </a>' +
+        '                                    </li>\n' +
+        '                                </ul>\n' +
+        '                            </div>\n' +
+        '                            <div class="col" id="questionplace-1">\n' +
+        '                                <div id="answer-body">\n' +
+        '                                    <p>' + item.body + '<br></p>\n' +
+        '                                </div>\n' +
+        '                                <div id="users-info-1">\n' +
+        '                                    <div class="row">\n' +
+        '                                        <div class="col">\n' +
+        '                                            <div style="display: inline;">\n' +
+        '                                                <div><a href="#"><small class="text-secondary">Править</small></a></div>\n' +
+        '                                            </div>\n' +
+        '                                        </div>\n' +
+        '                                        <div class="col"></div>\n' +
+        '                                        <div class="col" style="background-color:lightblue; padding: .2rem .5rem; border-radius: .2rem; text-decoration: none">\n' +
+        '                                            <div><small>Ответ: ' + timeDifference(item.persistDate) + '</small></div>\n' +
+        '                                            <div></div>\n' +
+        '                                            <ul class="list-unstyled">\n' +
+        '                                                <li><i class="fa fa-star"></i><small>' + item.nickName + '</small></li>\n' +
+        '                                            </ul>\n' +
+        '                                        </div>\n' +
+        '                                    </div>\n' +
+        '                                </div>\n' +
+                                                //комментарии для js
+        '                                    <div className="post-layout--right js-post-comments-component" id="question-comments">'+
+        '                                        <div>'+
+        '                                            <small>'+
+        '                                                <ul className="comments-list js-comments-list" id="' + answerCommentsId + '">'+
+        '                                                </ul>'+
+        '                                            </small>'+
+        '                                        </div>'+
+        '                                    </div>'+
+        '                                <div id="' + buttonCommentsId + '" data-rep="50" data-reg="true">'+
+        '                                   <button type="button" className="btn btn-link btn-sm"'+
+        '                                            onClick="openCommentArea(`#' + buttonCommentsId + '`, false)">Добавить комментарий'+
+        '                                    </button>'+
+        '                                </div>'+
+        '                            </div>' +
+        '                        </div>' +
+        '                    </div>' +
+        '                    <hr>' +
+        '                </div>';
 }
 
 function getDateFromDateTime(dateTime) {
@@ -212,10 +235,51 @@ function votesNumber(data) {
     return !data ? 0 : data;
 }
 
-sendAnswer.addEventListener("click", function (e) {
-    let bodytext = document.querySelector('#questionBodyInput').value
+function fillQuestionComments() {
+    //Дополнить после появления API для вывода комментариев
+    // Пример реализации в addCommentToQuestionPOST
+    // let ul = document.querySelector(answerId);
+    // let li = document.createElement("li");
+    // li.appendChild(document.createTextNode("Text"));
+    // ul.appendChild(li);
+}
 
-    fetch('/api/user/question/' + questionId + '/answer/add',
+function fillCommentsToAnswer(answerId){
+    //Дополнить после появления API для вывода комментариев
+    // Пример реализации в addCommentToQuestionPOST
+    // console.log(answerId);
+    // let ul = document.querySelector(answerId);
+    // let li = document.createElement("li");
+    // li.appendChild(document.createTextNode(answerId));
+    // ul.appendChild(li);
+}
+
+function openCommentArea(commentAreaId, isQuestion){
+    if (isQuestion) {
+        document.querySelector(commentAreaId).innerHTML =
+            '<div>' +
+            '   <br>' +
+            '   <textarea className="editor" id="comment-to-question-body" rows="2" required style="width: 100%"></textarea>' +
+            '</div>' +
+            '<div id="add-comment-to-question" data-rep="50" data-reg="true">' +
+            '   <button type="button" class="btn btn-primary btn-sm" onclick="addCommentToQuestionPOST()">Добавить комментарий</button>' +
+            '</div>'
+    } else {
+        let answerId = commentAreaId.split("-").slice(-1)[0];
+        document.querySelector(commentAreaId).innerHTML =
+            '<div>' +
+            '   <br>' +
+            '   <textarea className="editor" id="comment-to-answer-body-' + answerId + '" rows="2" required style="width: 100%"></textarea>' +
+            '</div>' +
+            '<div id="add-comment-to-question" data-rep="50" data-reg="true">' +
+            '   <button type="button" class="btn btn-primary btn-sm" onclick="addCommentToAnswerPOST(' + answerId + ')">Добавить комментарий</button>' +
+            '</div>'
+    }
+}
+
+function addCommentToQuestionPOST() {
+    let bodytext = document.querySelector('#comment-to-question-body').value;
+    fetch('/api/user/question/' + questionId + '/comment',
         {
             method: 'POST',
             headers: {
@@ -224,55 +288,62 @@ sendAnswer.addEventListener("click", function (e) {
             },
             body: JSON.stringify(bodytext)
         }).then(response => {
-            if (response.status === 200) {
-                window.location.reload();
-            } else if (response.status === 400) {
-                alert("Ошибка добавления ответа: на данный вопрос пользователь уже отвечал.")
-            } else if (response.status === 404) {
-                alert("Ошибка добавления ответа: вопрос не найден.")
+            if (response.status != 200) {
+                if (response.status === 400) {
+                    alert("Пустой комментарий")
+                } else if (response.status === 404) {
+                    alert("Вопрос не найден в системе")
+                }
+            } else {
+                response.json().then(data => {
+                    let ul = document.querySelector("#questionComments");
+                    let li = document.createElement("li");
+                    li.appendChild(document.createTextNode(data.comment));
+                    li.setAttribute("id", "comment-" + data.id);
+                    ul.appendChild(li);
+                })
             }
         })
-})
+    document.querySelector("#add-comment-to-question").innerHTML =
+        '<button type="button" class="btn btn-link btn-sm" ' +
+        'onclick="openCommentArea(`#add-comment-to-question`, true)">' +
+        'Добавить комментарий' +
+        '</button>'
+}
 
-addCommentToQuestion.addEventListener('click', function (e) {
-    if (document.querySelector('#question-add-comment-pool').innerHTML === "") {
-        document.querySelector('#question-add-comment-pool').innerHTML =
-            '<div>' +
-            '   <br>' +
-            '   <textarea className="editor" id="comment-to-question-body" rows="2" required style="width: 100%"></textarea>' +
-            '</div>';
-        document.querySelector('#question-add-comment-button').innerHTML =
-            '<button type="submit" className="btn btn-secondary my-3" id="handlecomment">Добавить комментарий</button>';
-    } else {
-        let bodytext = document.querySelector('#comment-to-question-body').value
-
-        fetch('/api/user/question/' + questionId + '/comment',
-            {
-                method: 'POST',
-                headers: {
-                    'Authorization': 'Bearer ' + getCookie('token'),
-                    'Accept': 'application/json', 'Content-Type': 'application/json'
-                },
-                body: JSON.stringify(bodytext)
-            }).then(response => {
-                if (response.status != 200) {
-                    if (response.status === 400) {
-                        alert("Пустой комментарий")
-                    } else if (response.status === 404) {
-                        alert("Вопрос не найден в системе")
-                    }
-                } else {
-                    response.json().then(data => {
-                        console.log(data);
-                        alert("Комментарий добавлен")
-                        // window.location.reload();
-                    })
-                }
-        })
-        document.querySelector('#question-add-comment-pool').innerHTML ="";
-        document.querySelector('#question-add-comment-button').innerHTML =
-            '<a href="#">'+
-            '   <small class="text-secondary" id="add-comment-to-question">Добавить комментарий</small>'+
-            '</a>    '
-    }
-})
+function addCommentToAnswerPOST(answerNumber) {
+    let textBodyId = "#comment-to-answer-body-" + answerNumber;
+    let bodytext = document.querySelector(textBodyId).value;
+    fetch('/api/user/question/' + questionId + '/answer/' + answerNumber + '/comment',
+        {
+            method: 'POST',
+            headers: {
+                'Authorization': 'Bearer ' + getCookie('token'),
+                'Accept': 'application/json', 'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(bodytext)
+        }).then(response => {
+        if (response.status != 200) {
+            if (response.status === 400) {
+                alert("Пустой комментарий")
+            } else if (response.status === 404) {
+                alert("Вопрос или ответ не найдены в системе")
+            }
+        } else {
+            response.json().then(data => {
+                let commentsId = "#anwserComments-" + answerNumber;
+                let ul = document.querySelector(commentsId);
+                let li = document.createElement("li");
+                li.appendChild(document.createTextNode(data.comment));
+                li.setAttribute("id", "comment-" + data.id);
+                ul.appendChild(li);
+            })
+        }
+    })
+    let answerId = "#buttonAnswerComments-" + answerNumber;
+    document.querySelector(answerId).innerHTML =
+        '<button type="button" class="btn btn-link btn-sm" ' +
+        'onclick="openCommentArea(`' + answerId + '`, false, )">' +
+        'Добавить комментарий' +
+        '</button>'
+}
