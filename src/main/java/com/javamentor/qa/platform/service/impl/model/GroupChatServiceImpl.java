5b5e5ccc267bb06c0c2c3555be230fdc3e8422a9
KScom <redomi@ya.ru>
2022-09-01 21:28:05 +0300

    ver 2

--- src/main/java/com/javamentor/qa/platform/service/impl/model/GroupChatServiceImpl.java
+++ src/main/java/com/javamentor/qa/platform/service/impl/model/GroupChatServiceImpl.java
@@ -8,7 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.stereotype.Service;
 
-import org.springframework.transaction.annotation.Transactional;
+import javax.transaction.Transactional;
 import java.util.Optional;
 
 @Service
