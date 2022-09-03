581860a6621b8aab62fe36a084f441973e047fe3
KScom <redomi@ya.ru>
2022-09-03 20:53:42 +0300

    create entity UserChatPin, ChatDao and ChatService

---
+++ src/main/java/com/javamentor/qa/platform/models/entity/user/UserChatPin.java
@@ -0,0 +1,65 @@
+package com.javamentor.qa.platform.models.entity.user;
+
+import com.javamentor.qa.platform.models.entity.chat.Chat;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.Type;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+import java.time.LocalDateTime;
+import java.util.Objects;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "user_chat_pin")
+public class UserChatPin {
+
+    @Id
+    @GeneratedValue(generator = "user_chat_pin_seq")
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @JoinColumn(name = "chat_id")
+    private Chat chat;
+
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    @Column(name = "persist_date", nullable = false, updatable = false)
+    @Type(type = "org.hibernate.type.LocalDateTimeType")
+    @CreationTimestamp
+    private LocalDateTime persistDate;
+
+    public UserChatPin(Chat chat, User user) {
+        this.chat = chat;
+        this.user = user;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        UserChatPin that = (UserChatPin) o;
+        return Objects.equals(id, that.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+
+}
---
+++ src/main/java/com/javamentor/qa/platform/service/impl/model/ChatServiceImpl.java
@@ -0,0 +1,21 @@
+package com.javamentor.qa.platform.service.impl.model;
+
+import com.javamentor.qa.platform.dao.abstracts.model.ChatDao;
+import com.javamentor.qa.platform.models.entity.chat.Chat;
+import com.javamentor.qa.platform.service.abstracts.model.ChatService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ChatServiceImpl extends ReadWriteServiceImpl<Chat, Long> implements ChatService {
+
+    private ChatDao chatDao;
+
+    @Autowired
+    public ChatServiceImpl(ChatDao chatDao) {
+        super(chatDao);
+        this.chatDao = chatDao;
+    }
+
+
+}
