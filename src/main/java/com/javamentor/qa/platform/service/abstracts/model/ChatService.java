0d498aff8b737353e661a1653011e10716854704
RomanLoriia <loria.ro@yandex.ru>
2022-09-09 14:29:57 +0300

    messageStarController-add

--- src/main/java/com/javamentor/qa/platform/service/abstracts/model/ChatService.java
+++ src/main/java/com/javamentor/qa/platform/service/abstracts/model/ChatService.java
@@ -3,6 +3,5 @@ package com.javamentor.qa.platform.service.abstracts.model;
 import com.javamentor.qa.platform.models.entity.chat.Chat;
 
 public interface ChatService extends ReadOnlyService<Chat, Long> {
-
-
+    boolean isChatHasUser(Long chatId, Long userId);
 }
